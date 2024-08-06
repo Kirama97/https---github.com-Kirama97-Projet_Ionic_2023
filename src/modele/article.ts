a022d9c12ddb02b8143f674fcb064de868e52da2
Kirama97 <officialthiam97@gmail.com>
2024-08-06 16:42:59 +0000

    commit

--- src/modele/article.ts
+++ src/modele/article.ts
@@ -2,12 +2,10 @@
 export interface ARTICLE {
     id:string ;
     titre : string ;
-    Marque:string
     description:string ;
     image:string[] ; 
     prix : number  ; 
     categorie:string ;
-    state : string;
     date : Date;
     region:string ;
     etoile:number[];
